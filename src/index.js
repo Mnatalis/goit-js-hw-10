54ba0fc04261faf57c584c18ad4a44ca3900e8eb
Nataliss <oksklient@gmail.com>
2023-05-06 16:05:41 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -14,20 +14,31 @@ input.addEventListener(
   debounce(e => {
     const trimmedValue = input.value.trim();
     cleanHtml();
+
     if (trimmedValue !== '') {
-      fetchCountries(trimmedValue).then(foundData => {
-        if (foundData.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
+      fetchCountries(trimmedValue)
+        .then(foundData => {
+          if (foundData.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (foundData.length === 0) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+          } else if (foundData.length >= 2 && foundData.length <= 10) {
+            renderCountryList(foundData);
+          } else if (foundData.length === 1) {
+            renderOneCountry(foundData);
+          }
+        })
+        .catch(error => {
+          if (error.message === '404') {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+            return;
+          }
+          Notiflix.Notify.warning(
+            'Oops, some unexpected error, pls. try againe'
           );
-        } else if (foundData.length === 0) {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (foundData.length >= 2 && foundData.length <= 10) {
-          renderCountryList(foundData);
-        } else if (foundData.length === 1) {
-          renderOneCountry(foundData);
-        }
-      });
+        });
     }
   }, DEBOUNCE_DELAY)
 );
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,18 +1,11 @@
-const fetchCountries = name => {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
-};
-export { fetchCountries };
+export function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,languages,population,flags
+`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+}
